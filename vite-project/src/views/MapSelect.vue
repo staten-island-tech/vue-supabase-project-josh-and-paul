3841e73b768e14e408d3a39d05fe54bffc4055d9
JoshRov2 <joshr16@nycstudents.net>
2024-04-16 09:21:24 -0400

    Update MapSelect.vue

--- vite-project/src/views/MapSelect.vue
+++ vite-project/src/views/MapSelect.vue
@@ -1,13 +1,14 @@
 <script setup>
 </script>
 <template>
-    <div>
-        <img class="map" src="../assets/map.png" alt="map">
+    <div class="map">
+        <button class="freshwater">Freshwater Fishing</button>
+        <button class="inshore">Inshore Saltwater Fishing</button>
+        <button class="offshore">Offshore Saltwater Fishing</button>
     </div>
 </template>
 <style>
 .map{
-    height: 100vh;
-    width: 100vw;
+    background-image: url("../assets/map.png");
 }
 </style>
