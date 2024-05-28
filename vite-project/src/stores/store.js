26078b796a401cf0051f9f0941e91a4e813781f7
JoshRov2 <joshr16@nycstudents.net>
2024-05-28 08:57:52 -0400

    Update store.js

--- vite-project/src/stores/store.js
+++ vite-project/src/stores/store.js
@@ -1,3 +1,6 @@
+<<<<<<< HEAD
+=======
+<<<<<<< Updated upstream
 import { defineStore } from 'pinia'
 
 export const useStore = defineStore('storeId', {
@@ -25,3 +28,37 @@ export const useStore = defineStore('storeId', {
       }
     },
   }) */
+=======
+import { ref } from 'vue'
+>>>>>>> parent of 1426c15 (5/28)
+import { defineStore } from 'pinia'
+
+export const useStore = defineStore('storeId', {
+  // arrow function recommended for full type inference
+  state: () => {
+    return {
+      // all these properties will have their type inferred automatically
+      count: 0,
+      name: 'Eduardo',
+      isAdmin: true,
+      items: [],
+      hasChanged: true,
+      userList: [],
+      // for data that is not yet loaded
+      user: null ,
+    }
+  },
+})
+<<<<<<< HEAD
+
+/* export const useUserStore = defineStore('user', {
+    state: () => {
+      return {
+        userList: [],
+        user: null ,
+      }
+    },
+  }) */
+=======
+>>>>>>> Stashed changes
+>>>>>>> parent of 1426c15 (5/28)
