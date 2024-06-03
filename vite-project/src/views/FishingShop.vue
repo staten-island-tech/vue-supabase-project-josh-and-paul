7525b850a3a5d102e049f08c4cfef16f0a4edab6
paulmkrtichian <paulmkr22@gmail.com>
2024-06-03 09:22:08 -0400

    6/3

---
+++ vite-project/src/views/FishingShop.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="fishingshop-container">
+  <div v-for="lure in lures" :key="lure.type" class="lure-box">
+  <h1>{{ lure.type }}</h1>
+  <h2>{{ lure.description }}</h2>
+  <img :src="lure.img"  />
+  <h2>{{ lure.price }}</h2>
+  <button id="buttton1" @click = "addaddedcartocart(lure)">Add To Cart</button> 
+  </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
